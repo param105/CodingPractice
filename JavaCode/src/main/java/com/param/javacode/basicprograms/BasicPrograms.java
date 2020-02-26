ea5570b749884ab72b2a471fdc234a30e6013f54
Parmeshwar <parmeshwar@bruviti.com>
2020-02-26 09:47:55 +0530

    folder name changed

--- JavaCode/src/main/java/com/param/javacode/basicprograms/BasicPrograms.java
+++ JavaCode/src/main/java/com/param/javacode/basicprograms/BasicPrograms.java
@@ -1,4 +1,4 @@
-package com.param.javacode.stringprograms;
+package com.param.javacode.basicprograms;
 
 public class BasicPrograms {
     private static int[] intArray = {0,1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,100};
