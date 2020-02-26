8cb5a6ca0738fcee4c26062e61251abe4124f4d2
Parmeshwar <parmeshwar@bruviti.com>
2020-02-26 10:29:53 +0530

    reverse word program added

--- JavaCode/src/main/java/com/param/javacode/basicprograms/BasicPrograms.java
+++ JavaCode/src/main/java/com/param/javacode/basicprograms/BasicPrograms.java
@@ -1,13 +1,68 @@
 package com.param.javacode.basicprograms;
 
+import java.util.HashMap;
+
 public class BasicPrograms {
     private static int[] intArray = {0,1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,100};
+    private static char[] charArray = {'p','a','r','m','e','s','h','w','a','r','c','h','a','v','a','n'};
 
     public static void main(String[] args) {
+        System.out.println("### Check string as palindrome >>>> ");
         isStringPalindrome();//check if String is palindrome or not
-        reverseEachWord(); // reverse each word from sentence
+        System.out.println( "\n-------------------------------------------");
+
+        System.out.println("### Find letters count form the array >>>> ");
         findLettersCount(); // find letter count
+        System.out.println( "\n-------------------------------------------");
+
+        System.out.println("### Find odd numbers without for loop >>>> ");
         findOddNumbersFromArray(intArray,0); // find odd numbers
+        System.out.println( "\n-------------------------------------------");
+
+
+        System.out.println("### Reverse each word from the string >>>> ");
+        reverseEachWordFromSentence(); // reverse each word from sentence
+        System.out.println( "\n-------------------------------------------");
+
+    }
+
+
+    /***
+     * to check if string is palindrome
+     */
+    private static void isStringPalindrome() {
+        String str = "maddam";
+        boolean isPalindrome = true;
+        int i,j =0;
+
+        for ( i = 0, j= str.length()-1; i <= str.length()/2 && j >=  (str.length())/2 ; i++,j--) {
+            if(str.charAt(i) != str.charAt(j)){
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome)
+            System.out.println(str+" is palindrome");
+        else
+            System.out.println(str+" is NOT palindrome");
+
+    }
+
+    /***
+     * find each letters count from the string or array.
+     */
+    private static void findLettersCount() {
+        HashMap<Character,Integer> letterCoutMap = new HashMap<>();
+        for (Character c: charArray) {
+            if (letterCoutMap.containsKey(c)) {
+                letterCoutMap.put(c, letterCoutMap.get(c) + 1);
+            } else {
+                letterCoutMap.put(c, 1);
+            }
+        }
+        for (Character c: letterCoutMap.keySet()) {
+            System.out.println( c +" -> "+ letterCoutMap.get(c));
+        }
 
     }
 
@@ -24,40 +79,26 @@ public class BasicPrograms {
     }
 
     /***
-     * find each letters count from the string or array.
-     */
-    private static void findLettersCount() {
-
-    }
-
-    /***
      * Sentense - Hello World
      * ans - olleH dlroW
      */
-    private static void reverseEachWord() {
+    private static void reverseEachWordFromSentence() {
+        String str = "I am Android Engineer.";
+        StringBuilder sb = new StringBuilder();
 
-    }
-
-    /***
-     * to check if string is palindrome
-     */
-    private static void isStringPalindrome() {
-        String str = "maddam";
-        boolean isPalindrome = true;
-        int i,j =0;
-
-        for ( i = 0, j= str.length()-1; i <= str.length()/2 && j >=  (str.length())/2 ; i++,j--) {
-                if(str.charAt(i) != str.charAt(j)){
-                    isPalindrome = false;
-                    break;
+        for (char c: str.toCharArray()) {
+            if(c == ' ' || c == '.'){
+                for (int i = sb.length()-1; i >=0 ; i--) {
+                    System.out.print(sb.charAt(i));
                 }
+                System.out.println(" ");
+                sb.delete(0,sb.length());
+            }else{
+                sb.append(c);
+            }
         }
-        if(isPalindrome)
-            System.out.println(str+" is palindrome");
-        else
-            System.out.println(str+" is NOT palindrome");
+        System.out.println(" ");
 
     }
-
 
 }
