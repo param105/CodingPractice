a7514b85dd071f74a41f00acf495522050c27d4d
Parmeshwar <parmeshwar@bruviti.com>
2020-03-07 16:54:43 +0530

    generics with wildcard demo added

--- JavaCode/src/main/java/com/param/javacode/generics/GenericsDemo.java
+++ JavaCode/src/main/java/com/param/javacode/generics/GenericsDemo.java
@@ -1,13 +1,29 @@
 package com.param.javacode.generics;
 
+/***
+ * class showing generics demo with T, E, V as generic values
+ * @param <T>
+ * @param <E>
+ */
 class GenericsDemo<T,E> {
 
     private T obj1;
     private E obj2;
+    private double doubleVal;
 
+    /***
+     * Generic type parameters
+     */
     GenericsDemo(T obj, E obj2){
         this.obj1 = obj;
         this.obj2 = obj2;
+    }
+
+    /***
+     * Generic constructor can be used even if class is not generic
+     */
+    <V extends Number> GenericsDemo(V val){
+        doubleVal = val.doubleValue();
     }
 
     T getObj1(){
@@ -16,6 +32,10 @@ class GenericsDemo<T,E> {
 
     E getObj2(){
         return obj2;
+    }
+
+    void showVal(){
+        System.out.println("Value : "+ doubleVal);
     }
 
     String getType(){
@@ -32,5 +52,7 @@ class MainClass{
         GenericsDemo<String,String> stringObj = new GenericsDemo<>("Param","Chavan");
         System.out.println("values in "+ stringObj.getType() +" object are :"+ stringObj.getObj1() +","+ stringObj.getObj2());
 
+        GenericsDemo doubleGeneric = new GenericsDemo(555);
+        doubleGeneric.showVal();
     }
 }
